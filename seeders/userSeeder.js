9406fdd6079a10270e2abcb65d45d72aa200397c
SebaPrado <sebastian.pradomelesi@gmail.com>
2024-03-18 11:30:10 -0300

    seba

--- seeders/userSeeder.js
+++ seeders/userSeeder.js
@@ -19,7 +19,7 @@ const User = require("../models/User");
 async function userSeeder() {
   const users = [];
 
-  for (let i = 0; i < 10; i++) {
+  for (let i = 0; i < 3; i++) {
     users.push({
       nombre: faker.person.firstName(),
       apellido: faker.person.lastName(),
